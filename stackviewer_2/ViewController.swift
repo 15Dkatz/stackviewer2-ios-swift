53dde9053744eaea014d14318dd8b1509df9b046
David Katz <15Dkatz@shcp.edu>
2015-09-22 17:25:43 -0700

    Initial Commit

---
+++ stackviewer_2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stackviewer_2
+//
+//  Created by David Katz on 9/22/15.
+//  Copyright © 2015 davidkatz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
